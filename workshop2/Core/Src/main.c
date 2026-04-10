b9c6b2db51256ef7c8d7df91c296ffbd0f170731
yaroslav1211 <vodopyanov.yaroslav@lll.kpi.ua>
2026-04-10 10:10:35 +0300

    workshop2

--- workshop2/Core/Src/main.c
+++ workshop2/Core/Src/main.c
@@ -91,6 +91,11 @@ int main(void)
   MX_TIM4_Init();
   /* USER CODE BEGIN 2 */
 
+  HAL_TIM_PWM_Start(&htim4,TIM_CHANNEL_1);
+  HAL_TIM_PWM_Start(&htim4,TIM_CHANNEL_2);
+  HAL_TIM_PWM_Start(&htim4,TIM_CHANNEL_3);
+  HAL_TIM_PWM_Start(&htim4,TIM_CHANNEL_4);
+
   /* USER CODE END 2 */
 
   /* Infinite loop */
